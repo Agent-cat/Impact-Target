09dd14cb312d32318f73b74b51ff735b541461eb
Agent-cat <2300031042@kluniversity.in>
2026-02-13 18:51:49 +0530

    test-3

--- lib/todoFilter.ts
+++ lib/todoFilter.ts
@@ -14,6 +14,7 @@ export function filterTodos(todos: Todo[], filter: FilterType): Todo[] {
     default:
       return todos;
   }
+
 }
 
 export function sortTodosByDate(todos: Todo[], ascending: boolean = true): Todo[] {
