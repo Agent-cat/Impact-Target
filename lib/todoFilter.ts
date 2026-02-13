2c68623dac59384b364233fd028fcc86ab83de74
Agent-cat <2300031042@kluniversity.in>
2026-02-13 19:11:36 +0530

    one

--- lib/todoFilter.ts
+++ lib/todoFilter.ts
@@ -7,8 +7,6 @@ export function filterTodos(todos: Todo[], filter: FilterType): Todo[] {
     case "active":
       return todos.filter((t) => !t.completed);
     case "completed":
-      console.log("Completed todos:", todos.filter((t) => t.completed));
-
       return todos.filter((t) => t.completed);
     case "all":
     default:
